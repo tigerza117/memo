5afa7ed51963fda2444f1b092e955bf5e61afe9e
tigerza117 <noppawanza1171@gmail.com>
2021-06-06 09:42:08 +0700

    style: change some style

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ export const App = () => (
                   </Text>
               </Box>
           </Flex>
-          <Flex w={"50%"} h={"100%"}>
+          <Flex  h={"100%"}>
               <Me />
           </Flex>
       </Flex>
@@ -50,8 +50,7 @@ export const App = () => (
 )
 
 const Me = () => {
-    return <Box  margin={"auto"}  position={"relative"} transformOrigin={"center"}>
-
+    return <Box  margin={"auto"}  position={"relative"} >
         <Image zIndex={-1} maxW={"150%"} position={"absolute"} top={0} src={vector1} alt="Vector" />
         <Image src={vector2} alt="Vector" />
         <Image position={"absolute"} top={0} src={me} alt="Me" />
@@ -61,8 +60,8 @@ const Me = () => {
 
 const Logos = () => {
     return <>
-        <Box bg={"white"} p={6} boxShadow={"xl"} top={-10} position={"absolute"} rounded={"lg"}>
-            <Image src={vueLogo} alt="Logo" />
+        <Box bg={"white"} p={6} boxShadow={"xl"} top={10} position={"absolute"} rounded={"lg"}>
+            <Image w={"60px"} src={vueLogo} alt="Logo" />
         </Box>
         <Box bg={"white"} p={6} boxShadow={"xl"} right={0} top={0} position={"absolute"} rounded={"lg"}>
             <Image src={jsLogo} alt="Logo" />
